6a46f4da98fa5c84c40a02b4238b90e26ace745f
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-12-18 20:58:19 -0500

    initial files

---
+++ src/Board.test.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+import { render } from "@testing-library/react";
+import Board from "./Board";
+
+it("renders without crashing", function () {
+  render(<Board />);
+});
